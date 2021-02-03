94a0ae63a16ea1c0f1308cdc4456f51f9f2dbc29
Heng <hzhan274@uOttawa.ca>
2021-02-02 22:12:27 -0500

    schema created

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # COVID-19_TRACKING
+
+Database: PostgreSQL | Version Control: Github & Git
---
+++ database_schema.sql
@@ -0,0 +1,28 @@
+CREATE TABLE Covid_Fact_Table (
+	case_number int,
+	area_number int,
+	date_confirmed date,
+	PRIMARY KEY (case_number, area_number, date_confirmed)
+)
+
+CREATE TABLE PositiveCase(
+	case_number int PRIMARY KEY,
+	age int NOT NULL,
+	gender varchar(1) NOT NULL,
+	source_of_infection varchar(20),
+	test_date date NOT NULL,
+	area_number int
+)
+
+CREATE TABLE CityDailyInfo(
+	area_number int,
+	updated_date date,
+	testing_facilities_type varchar(20),
+	availability_of_facilities int,
+	total_cases int,
+	weather varchar(20),
+	gathering_area_type varchar(20),
+	mobility_info varchar(20),
+	average_stay_duration int,
+	PRIMARY KEY (area_number, updated_date)
+)
