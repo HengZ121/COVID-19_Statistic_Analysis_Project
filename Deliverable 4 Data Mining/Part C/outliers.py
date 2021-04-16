778247963cb8cd57ed5d26cd0cd0422858c6c131
Heng <hzhan274@uOttawa.ca>
2021-04-16 11:59:11 -0400

    Project Finished

--- Deliverable 4 Data Mining/Part C/outliers.py
+++ Deliverable 4 Data Mining/Part C/outliers.py
@@ -1,10 +1,16 @@
-import psycopg2
-import time
-from sklearn.ensemble import GradientBoostingClassifier
-import matplotlib.pyplot as plt
-from sklearn import svm
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.font_manager
+import psycopg2
 from scipy import stats
+
+from sklearn import svm
+from sklearn.covariance import EllipticEnvelope
+
+# Default settings
+n_samples = 200
+outliers_fraction = 0.25
+clusters_separation = [0, 1, 2]
 
 #establishing the connection to database
 conn = psycopg2.connect(
@@ -39,10 +45,13 @@ cursor.execute(
 		f.weather_key = w.weather_key AND f.case_number = c.case_number''')
 
 # algo & variables to define outliers
-algo =  ("One-Class SVM", svm.OneClassSVM(nu=0.25, kernel="rbf",
-                                      gamma=0.1))
-ground_truth = np.ones(200, dtype=int)
-ground_truth[-50:] = 0
+classifier =  [("One-Class SVM", svm.OneClassSVM(nu=0.25, kernel="rbf",
+                                      gamma=0.1))]
+xx, yy = np.meshgrid(np.linspace(-7, 7, 500), np.linspace(-7, 7, 500))
+n_inliers = int((1. - outliers_fraction) * n_samples)
+n_outliers = int(outliers_fraction * n_samples)
+ground_truth = np.ones(n_samples, dtype=int)
+ground_truth[-n_outliers:] = 0
 
 ### Divide instances into training and testing
 training = []
@@ -50,88 +59,66 @@ label_of_training = []
 testing = []
 label_of_testing = []
 counter = 0
-age_of20 = 0
-age_of10 = 0
-age_of30 = 0
-age_of40 = 0
-age_of50 = 0
-age_of60 = 0
 
+age_of20 = 0
 
 for record in cursor:
 	if record[10] == 20:
 		age_of20 = age_of20 + 1
-	if record[10] == 10:
-		age_of10 = age_of10 + 1
-	if record[10] == 30:
-		age_of30 = age_of30 + 1
-	if record[10] == 40:
-		age_of40 = age_of40 + 1
-	if record[10] == 50:
-		age_of50 = age_of50 + 1
-	if record[10] == 60:
-		age_of60 = age_of60 + 1
-
 	if (counter % 3 == 0):
 		if record[10] >= 60:
-			training.append(list(record[0:10]))
+			training.append(record[5])
 			label_of_training.append(record[10])
 		else:
-			testing.append(list(record[0:10]))
+			testing.append(record[5])
 			label_of_testing.append(record[10])
 	else:
 		### age distributing in training class without controlling imbalance: 10s - 21874, 20s - 32157, 30s - 25066, 40s - 22550, 50s - 22945, 60s - 14257
 		if record[10] != 20 or age_of20 < 25000:
-			training.append(list(record[0:10]))
+			training.append(record[5])
 			label_of_training.append(record[10])
 	counter = counter + 1
 
-starttime = time.time()
-clf = GradientBoostingClassifier(n_estimators=100, learning_rate=1.0, max_depth=1, random_state=0)
-clf = clf.fit(training, label_of_training)
-endtime = time.time()
 
-y_pred = clf.decision_function(training).ravel()
-threshold = stats.scoreatpercentile(y_pred,
-                                    100 * 0.25)
-y_pred = y_pred > threshold
-n_errors = (y_pred != ground_truth).sum()
-# plot the levels lines and the points
-Z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()])
-Z = Z.reshape(xx.shape)
-subplot = plt.subplot(1, 2, i + 1)
-subplot.set_title("Outlier detection")
-subplot.contourf(xx, yy, Z, levels=np.linspace(Z.min(), threshold, 7),
-                 cmap=plt.cm.Blues_r)
-a = subplot.contour(xx, yy, Z, levels=[threshold],
-                    linewidths=2, colors='red')
-subplot.contourf(xx, yy, Z, levels=[threshold, Z.max()],
-                 colors='orange')
-b = subplot.scatter(X[:-n_outliers, 0], X[:-n_outliers, 1], c='white')
-c = subplot.scatter(X[-n_outliers:, 0], X[-n_outliers:, 1], c='black')
+
+
+# Fit the problem with varying cluster separation
+for i, offset in enumerate(clusters_separation):
+    X = np.r_['1,2,0' ,training, label_of_training]
+
+    # Fit the model with the One-Class SVM
+    n_errors = 0
+    plt.figure(figsize=(10, 5))
+    for i, (clf_name, clf) in enumerate(classifier):
+        # fit the data and tag outliers
+        clf.fit(X)
+        y_pred = clf.decision_function(X).ravel()
+        threshold = stats.scoreatpercentile(y_pred,
+                                            100 * outliers_fraction)
+        y_pred = y_pred > threshold
+        if y_pred != ground_truth:
+        	n_errors = n_errors + 1
+        # plot the levels lines and the points
+        Z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()])
+        Z = Z.reshape(xx.shape)
+        subplot = plt.subplot(1, 2, i + 1)
+        subplot.set_title("Outlier detection")
+        subplot.contourf(xx, yy, Z, levels=np.linspace(Z.min(), threshold, 7),
+                         cmap=plt.cm.Blues_r)
+        a = subplot.contour(xx, yy, Z, levels=[threshold],
+                            linewidths=2, colors='red')
+        subplot.contourf(xx, yy, Z, levels=[threshold, Z.max()],
+                         colors='orange')
+        b = subplot.scatter(X[:-n_outliers, 0], X[:-n_outliers, 1], c='white')
+        c = subplot.scatter(X[-n_outliers:, 0], X[-n_outliers:, 1], c='black')
+        subplot.axis('tight')
+        subplot.legend(
+            [a.collections[0], b, c],
+            ['learned decision function', 'true inliers', 'true outliers'],
+            prop=matplotlib.font_manager.FontProperties(size=11))
+        subplot.set_xlabel("%d. %s (errors: %d)" % (i + 1, clf_name, n_errors))
+        subplot.set_xlim((-7, 7))
+        subplot.set_ylim((-7, 7))
+    plt.subplots_adjust(0.04, 0.1, 0.96, 0.94, 0.1, 0.26)
 
 plt.show()
-TP = 0 ### correctly predicted cases whose age are below 40
-FN = 0 ### cases mis-predicted age > 40
-FP = 0 ### cases mis-predicted age < 40
-TN = 0 ### correctly predicted cases whose age > 40
-
-
-
-
-count = 0
-for elem in clf.predict(testing):
-	if label_of_testing[count] <= 40 and elem <= 40:
-		TP = TP + 1
-	elif label_of_testing[count] <= 40 and elem > 40:
-		FN = FN + 1
-	elif label_of_testing[count] > 40 and elem <= 40:
-		FP = FP + 1
-	else:
-		TN = TN + 1
-	count = count + 1
-
-print("Accuracy Rate: ", (TN + TP)/len(testing))
-print("Precision Rate: ", TP/(TP + FP))
-print("Recall Rate: ", TP/(TP + FN))
-print("Model Construction Time: ", (endtime - starttime), " sec")
